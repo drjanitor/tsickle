90dfd090cad753b7d725373a35c225a53708f111
Martin Probst <martin@probst.io>
2017-01-30 16:54:53 +0100

    Do not emit references to exported types in untyped mode.
    It turns out some build systems lose externs, so the references break.

---
+++ test_files/declare_export.untyped/declare_export_untyped.js
@@ -0,0 +1,1 @@
+goog.module('test_files.declare_export.untyped.declare_export_untyped');var module = module || {id: 'test_files/declare_export.untyped/declare_export_untyped.js'};
---
+++ test_files/declare_export.untyped/declare_export_untyped.ts
@@ -0,0 +1,3 @@
+export declare interface Foo {
+  bar: string;
+}
